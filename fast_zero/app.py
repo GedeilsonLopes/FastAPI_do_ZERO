788b9e5e0f191ce4b2a3c35ec09c31f96d16a791
Gedeilson Lopes <compasso-gedeison.lopes@ailos.coop.br>
2024-06-18 19:47:35 -0300

    Segunda aula: entendendo desenvolvimento web

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo'}
+
+
+@app.get('/OlaHtml', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_html():
+    return '''
+    <HTML>
+        <head>
+            <title> Nosso olá mundo </title>
+        </head>
+        <body>
+            <H1>Olá Mundo!</H1>
+        </body>
+    </HTML>
+'''
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,19 @@ def test_root_deve_retornar_ok_ola_mundo():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo'}
+
+
+def test_html_deve_retornar_ok_ola_mundo():
+    client = TestClient(app)
+    response = client.get('/OlaHtml')
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '''
+    <HTML>
+        <head>
+            <title> Nosso olá mundo </title>
+        </head>
+        <body>
+            <H1>Olá Mundo!</H1>
+        </body>
+    </HTML>
+'''
